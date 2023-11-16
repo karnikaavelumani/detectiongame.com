ec592c454bf086f8e19f771a38de0e2d820d21f7
karnikaavelumani <60043611+karnikaavelumani@users.noreply.github.com>
2023-11-15 18:00:28 -0800

    Fix styling

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,4 +1,6 @@
 import styles from "./hero.module.css";
+import Image from "next/image";
+import Background from "public/placeholder.webp";
 
 export default function Hero() {
   return (
@@ -6,6 +8,12 @@ export default function Hero() {
       <div className={styles.hero__content}>
         <h1 className={styles.hero__title}>Detection</h1>
       </div>
+      <Image
+        src={Background}
+        alt="Background video of gameplay"
+        className={styles.hero__image}
+        fill
+      />
     </section>
   );
 }
