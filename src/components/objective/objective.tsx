3552a828622191053f4b794cbfa2801df56aac55
karnikaavelumani <60043611+karnikaavelumani@users.noreply.github.com>
2025-02-04 12:12:09 -0800

    Objectives page complete

--- src/components/objective/objective.tsx
+++ src/components/objective/objective.tsx
@@ -6,11 +6,38 @@ export default function Objective() {
       <div className={styles.objective__content}>
         <h1 className={styles.objective__title}>OBJECTIVE</h1>
         <div className={styles.objective__container}>
-            <h2 className={styles.objective__item}>VR-FPS</h2>
-            <hr className={styles.objective__line} />
-            <p className={styles.objective__description}>
-            Players must physically crouch to navigate through levels, and peek around corners when fighting the enemy.
-            </p>
+          <h2 className={styles.objective__item}>VR-FPS</h2>
+          <hr className={styles.objective__line} />
+          <p className={styles.objective__description}>
+            Players must physically crouch to navigate through levels, and peek
+            around corners when fighting the enemy.
+          </p>
+        </div>
+        <div className={styles.objective__container}>
+          <h2 className={styles.objective__item}>LIDAR SCANNER</h2>
+          <hr className={styles.objective__line} />
+          <p className={styles.objective__description}>
+            The player robot perceives its environment by emitting LIDAR rays in
+            the direction it is facing. The color of the LIDAR rays change based
+            on the texture of the objects they land on.
+          </p>
+        </div>
+        <div className={styles.objective__container}>
+          <h2 className={styles.objective__item}>WEAPONS</h2>
+          <hr className={styles.objective__line} />
+          <p className={styles.objective__description}>
+            The player has access to various weapons such as rifles, shotguns,
+            pistols, and knives to eliminate enemies.
+          </p>
+        </div>
+        <div className={styles.objective__container}>
+          <h2 className={styles.objective__item}>MUSIC</h2>
+          <hr className={styles.objective__line} />
+          <p className={styles.objective__description}>
+            The beat and intensity of the music being played control various
+            elements such as the size of the LIDAR particles and the background
+            color of the scene.
+          </p>
         </div>
       </div>
     </section>
